28474ba1a1d34e8a3c29cf53dc3d0a78f564010a
René Mose Hansen <rmha63250@edu.ucl.dk>
2025-03-05 11:36:11 +0100

    Lavet readjsonfile

---
+++ Gotorz14/Services/ReadJsonFile.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Gotorz14.Services
+{
+    public static class ReadJsonFile
+    {
+        //readonly string Filepath;
+        //public ReadJsonFile()
+        //{
+        //    //var builder = new ConfigurationBuilder()
+        //    //.SetBasePath()
+        //    //.AddJsonFile("cheapestPerDay.json", optional: false, reloadOnChange: true);
+
+        //    //Configuration = builder.Build();
+
+        //}
+        public static string StringFromJsonFile()
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, "Services/cheapestPerDay.json");
+            string json = File.ReadAllText(path);
+            return json;
+        }
+
+
+
+    }
+}
--- Gotorz14/Services/ReneRod.cs
+++ Gotorz14/Services/ReneRod.cs
@@ -1,107 +1,109 @@
-﻿using System.Text.Json.Serialization;
-using System.Text.Json;
+﻿//using System.Text.Json.Serialization;
+//using System.Text.Json;
 
-namespace Gotorz14.Services
-{
-    public class ReneRod
-    {
-        NotImplementedException;
-    }
+//namespace Gotorz14.Services
+//{
+//    //public class ReneRod
+//    //{
+//    //    NotImplementedException;
+//    //}
 
-    public DateTime? ArrivalOfCheapestFlight(FareData fareData)
-    {
-        DateTime? ArrivalDateTimeOfCheapestFlight = fareData.Outbound.MinFare.ArrivalDate;
+//    public DateTime? ArrivalOfCheapestFlight(FareData fareData)
+//    {
+//        DateTime? ArrivalDateTimeOfCheapestFlight = fareData.Outbound.MinFare.ArrivalDate;
 
-        //Vælg den billigste og vælg ArrivalDate
-        //Brug denne til at søge efter hoteller fra den dato ...
+//        //Vælg den billigste og vælg ArrivalDate
+//        //Brug denne til at søge efter hoteller fra den dato ...
 
-        return ArrivalDateTimeOfCheapestFlight;
-    }
+//        return ArrivalDateTimeOfCheapestFlight;
+//    }
 
-    public static async Task<FareData> MakeFlightsList()
-    {
-        // URL til API
-        string url = "https://www.ryanair.com/api/farfnd/v4/oneWayFares/BER/BRU/cheapestPerDay?outboundMonthOfDate=2025-03-01&currency=EUR";
+//    public static async Task<FareData> MakeFlightsList()
+//    {
+//        // URL til API
+//        string url = "https://www.ryanair.com/api/farfnd/v4/oneWayFares/BER/BRU/cheapestPerDay?outboundMonthOfDate=2025-03-01&currency=EUR";
 
-        using (HttpClient client = new HttpClient())
-        {
-            // Udfør API-kaldet
-            HttpResponseMessage response = await client.GetAsync(url);
+//        using (HttpClient client = new HttpClient())
+//        {
+//            // Udfør API-kaldet
+//            HttpResponseMessage response = await client.GetAsync(url);
 
-            if (response.IsSuccessStatusCode)
-            {
-                // Læs JSON-svaret som en string
-                string jsonResponse = await response.Content.ReadAsStringAsync();
-                FareData fareData = JsonSerializer.Deserialize<FareData>(jsonResponse);
+//            if (response.IsSuccessStatusCode)
+//            {
+//                // Læs JSON-svaret som en string
 
 
-
-
-                return fareData;
-            }
-            else
-            {
-                return new FareData();
-            }
-        }
-    }
+//                string jsonResponse = await response.Content.ReadAsStringAsync();
+//                FareData fareData = JsonSerializer.Deserialize<FareData>(jsonResponse);
 
 
 
-    public class FareData
-    {
-        [JsonPropertyName("outbound")]
-        public Outbound Outbound { get; set; }
-    }
 
-    public class Outbound
-    {
-        [JsonPropertyName("fares")]
-        public List<Fare> Fares { get; set; }
+//                return fareData;
+//            }
+//            else
+//            {
+//                return new FareData();
+//            }
+//        }
+//    }
 
-        [JsonPropertyName("minFare")]
-        public Fare MinFare { get; set; }
 
-        [JsonPropertyName("maxFare")]
-        public Fare MaxFare { get; set; }
-    }
 
-    public class Fare
-    {
-        [JsonPropertyName("day")]
-        public string Day { get; set; }
+//    public class FareData
+//    {
+//        [JsonPropertyName("outbound")]
+//        public Outbound Outbound { get; set; }
+//    }
 
-        [JsonPropertyName("arrivalDate")]
-        public DateTime? ArrivalDate { get; set; }
+//    public class Outbound
+//    {
+//        [JsonPropertyName("fares")]
+//        public List<Fare> Fares { get; set; }
 
-        [JsonPropertyName("departureDate")]
-        public DateTime? DepartureDate { get; set; }
+//        [JsonPropertyName("minFare")]
+//        public Fare MinFare { get; set; }
 
-        [JsonPropertyName("price")]
-        public Price Price { get; set; }
+//        [JsonPropertyName("maxFare")]
+//        public Fare MaxFare { get; set; }
+//    }
 
-        [JsonPropertyName("soldOut")]
-        public bool SoldOut { get; set; }
+//    public class Fare
+//    {
+//        [JsonPropertyName("day")]
+//        public string Day { get; set; }
 
-        [JsonPropertyName("unavailable")]
-        public bool Unavailable { get; set; }
-    }
+//        [JsonPropertyName("arrivalDate")]
+//        public DateTime? ArrivalDate { get; set; }
 
-    public class Price
-    {
-        [JsonPropertyName("value")]
-        public double Value { get; set; }
+//        [JsonPropertyName("departureDate")]
+//        public DateTime? DepartureDate { get; set; }
 
-        [JsonPropertyName("valueMainUnit")]
-        public string ValueMainUnit { get; set; }
+//        [JsonPropertyName("price")]
+//        public Price Price { get; set; }
 
-        [JsonPropertyName("valueFractionalUnit")]
-        public string ValueFractionalUnit { get; set; }
+//        [JsonPropertyName("soldOut")]
+//        public bool SoldOut { get; set; }
 
-        [JsonPropertyName("currencyCode")]
-        public string CurrencyCode { get; set; }
+//        [JsonPropertyName("unavailable")]
+//        public bool Unavailable { get; set; }
+//    }
 
-        [JsonPropertyName("currencySymbol")]
-        public string CurrencySymbol { get; set; }
-    }
-}
+//    public class Price
+//    {
+//        [JsonPropertyName("value")]
+//        public double Value { get; set; }
+
+//        [JsonPropertyName("valueMainUnit")]
+//        public string ValueMainUnit { get; set; }
+
+//        [JsonPropertyName("valueFractionalUnit")]
+//        public string ValueFractionalUnit { get; set; }
+
+//        [JsonPropertyName("currencyCode")]
+//        public string CurrencyCode { get; set; }
+
+//        [JsonPropertyName("currencySymbol")]
+//        public string CurrencySymbol { get; set; }
+//    }
+//}
