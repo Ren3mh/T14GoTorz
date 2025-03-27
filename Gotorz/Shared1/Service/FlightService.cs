f519927acb80ac777c2612f51c3f1e04be256be3
René Mose Hansen <rmha63250@edu.ucl.dk>
2025-03-27 19:47:27 +0100

    FlightService ændringer

--- Gotorz/Shared1/Service/FlightService.cs
+++ Gotorz/Shared1/Service/FlightService.cs
@@ -23,6 +23,27 @@ namespace Shared.Service
                 .ToListAsync();
             return flights;
         }
+
+        //private readonly GotorzContext _context;
+
+        //public FlightService(GotorzContext context)
+        //{
+        //    _context = context;
+        //}
+
+        //public async Task Create(Gotorz14.Models1.Flight x)
+        //{
+        //    _context.Flights.Add(x);
+        //    await _context.SaveChangesAsync();
+        //}
+
+        //public async Task<List<Gotorz14.Models1.Flight>> GetAll()
+        //{
+        //    var flights = await _context.Flights
+        //        .ToListAsync();
+        //    return flights;
+        //}
+
     }
 
 }
