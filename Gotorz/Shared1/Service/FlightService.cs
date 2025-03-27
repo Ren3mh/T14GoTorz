21ebb9edfa5e42a2c325f9746283984d73479c70
René Mose Hansen <rmha63250@edu.ucl.dk>
2025-03-27 19:24:57 +0100

    Service Class added to Shared

---
+++ Gotorz/Shared1/Service/FlightService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Service
+{
+    public class FlightService : IFlightService
+    {
+
+        private readonly AppDbContext _context;
+
+        public FlightService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Flight>> GetAllFlights()
+        {
+            var flights = await _context.Flights
+                .Include(fp => fp.Outbound)
+                .Include(fp => fp.Homebound)
+                .ToListAsync();
+            return flights;
+        }
+    }
+
+}
