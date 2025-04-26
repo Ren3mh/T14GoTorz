29b837f6b6234a4d6e48eab766a911cb8acb5aac
elgaard99 <sean62359@edu.ucl.dk>
2025-04-26 18:21:01 +0200

    Address klasse er implementeret i koden, men databasen er ikke sat op.

--- GotorzApp/Shared/Address.cs
+++ GotorzApp/Shared/Address.cs
@@ -9,22 +9,83 @@ namespace Shared;
 
 public class Address
 {
-    [Required]
-    public string StreetNumber { get; set; }
+    private string streetNumber;
+    private string street;
+    private string city;
+    private string zipCode;
+    private string country;
+    private string? other;
 
     [Required]
-    public string Street { get; set; }
+    public string StreetNumber
+    {
+        get => streetNumber;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Street number cannot be null or empty.", nameof(StreetNumber));
+            streetNumber = value;
+        }
+    }
 
     [Required]
-    public string City { get; set; }
+    public string Street
+    {
+        get => street;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Street cannot be null or empty.", nameof(Street));
+            street = value;
+        }
+    }
 
     [Required]
-    public string ZipCode { get; set; }
+    public string City
+    {
+        get => city;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("City cannot be null or empty.", nameof(City));
+            city = value;
+        }
+    }
 
     [Required]
-    public string Country { get; set; }
+    public string ZipCode
+    {
+        get => zipCode;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Zip code cannot be null or empty.", nameof(ZipCode));
+            zipCode = value;
+        }
+    }
 
-    public string? Other { get; set; }
+    [Required]
+    public string Country
+    {
+        get => country;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Country cannot be null or empty.", nameof(Country));
+            country = value;
+        }
+    }
+
+    public string? Other
+    {
+        get => other;
+        set
+        {
+            if (value == "")
+                throw new ArgumentException("Other cannot be empty.", nameof(Other));
+            other = value;
+        }
+    }
 
     public Address(string streetNumber, string street, string city, string zipCode, string country, string? other = null)
     {
