991061c29dd076190b897c11a1a1dc1a19016aac
René Mose Hansen <rmha63250@edu.ucl.dk>
2025-04-30 13:54:05 +0200

    Added Componetn terst for PackageView

---
+++ GotorzApp/ComponentsTesting/PackageViewTest.cs
@@ -0,0 +1,29 @@
+﻿using Bunit;
+using GotorzApp.Components.Pages.Presentation;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Shared;
+using Shared.Service;
+using System.Collections.Generic;
+using System;
+using Xunit;
+
+namespace ComponentsTesting;
+
+public class PackageViewTest : TestContext
+{
+    private readonly Mock<IService<TravelPackage>> _mockPackageService;
+
+
+    public PackageViewTest()
+    {
+        _mockPackageService = new Mock<IService<TravelPackage>>();
+        Services.AddSingleton(_mockPackageService.Object);
+
+    }
+
+
+
+    
+}
