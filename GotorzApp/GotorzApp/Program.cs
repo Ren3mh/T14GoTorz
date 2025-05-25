3e555b9a6190c880316de0386883d96211297abd
elgaard99 <sean62359@edu.ucl.dk>
2025-05-25 19:07:25 +0200

    Photos.sql containing dummy photos added

--- GotorzApp/GotorzApp/Program.cs
+++ GotorzApp/GotorzApp/Program.cs
@@ -17,6 +17,7 @@ using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
